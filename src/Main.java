a5adb2a3fcc5f804e7774cb56b108f95533cd6de
SFS76 <sfs76@yandex.ru>
2024-06-30 11:34:10 +0400

    tray

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package hw2;
+public class Main {
+    public static void main(String[] args) {
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box = box * 3;
+        System.out.println(box);
+        box = box / 4;
+        System.out.println(box);
+
+                    }
+}
